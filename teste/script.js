34977624d1a4deed1755ac49ccc97ac9066b6279
transformar-educacao <matheus.pinho@senac.br>
2024-11-05 10:03:37 -0300

    Add files via upload

---
+++ teste/script.js
@@ -0,0 +1,24 @@
+const d = 40;
+document.querySelectorAll('.rocket-button').forEach(elem => {
+    elem.querySelectorAll('.default, .success > div').forEach(text => {
+        charming(text);
+        text.querySelectorAll('span').forEach((span, i) => {
+            span.innerHTML = span.textContent == ' ' ? '&nbsp;' : span.textContent;
+            span.style.setProperty('--d', i * d + 'ms');
+            span.style.setProperty('--ds', text.querySelectorAll('span').length * d - d - i * d + 'ms');
+        });
+    });
+    elem.addEventListener('click', e => {
+        e.preventDefault();
+        if (elem.classList.contains('animated')) {
+            return;
+        }
+        elem.classList.add('animated');
+        elem.classList.toggle('live');
+        
+        // Redirect to the form after animation
+        setTimeout(() => {
+            window.location.href = elem.getAttribute('href');
+        }, 2400);
+    });
+});
